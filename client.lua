e23da549b2918451b4725a31605c15374e2fb14a
Joe Szymkowicz <joe.szymkowicz@gmail.com>
2022-03-18 19:40:28 -0500

    Modernize i guess

--- client.lua
+++ client.lua
@@ -1,11 +1,4 @@
-QBCore = nil
-
-Citizen.CreateThread(function()
-	while QBCore == nil do
-		TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-		Citizen.Wait(0)
-	end
-end)
+local QBCore = exports['qb-core']:GetCoreObject()
 
 Config = {} 
 Config.DamageNeeded = 100.0 -- 100.0 being broken and 1000.0 being fixed a lower value than 100.0 will break it
@@ -14,29 +7,21 @@ Config.MaxHeight = 5.0
 Config.MaxLength = 5.0
 
 local Keys = {
-  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+  ["E"] = 38, ["A"] = 34, ["D"] = 9, ["LEFTSHIFT"] = 21
 }
 
 local First = vector3(0.0, 0.0, 0.0)
 local Second = vector3(5.0, 5.0, 5.0)
 
 local Vehicle = {}
+
 Citizen.CreateThread(function()
-    Citizen.Wait(3000)
     while true do
         local ped = PlayerPedId()
         local closestVehicle = QBCore.Functions.GetClosestVehicle()
         local vehicleCoords = GetEntityCoords(closestVehicle)
         local dimension = GetModelDimensions(GetEntityModel(closestVehicle), First, Second)
-        if DoesEntityExist(closestVehicle) and IsEntityAVehicle(closestVehicle) and not IsPedInAnyVehicle(ped, false) and #(GetEntityCoords(closestVehicle) - GetEntityCoords(PlayerPedId())) < 5.0 then
+        if DoesEntityExist(closestVehicle) and IsEntityAVehicle(closestVehicle) and not IsPedInAnyVehicle(ped, false) and #(GetEntityCoords(closestVehicle) - GetEntityCoords(ped)) < 5.0 then
             Vehicle.Coords = vehicleCoords
             Vehicle.Dimensions = dimension
             Vehicle.Vehicle = closestVehicle
@@ -68,9 +53,9 @@ Citizen.CreateThread(function()
                 NetworkRequestControlOfEntity(Vehicle.Vehicle)
                 local coords = GetEntityCoords(ped)
                 if Vehicle.IsInFront then    
-                    AttachEntityToEntity(PlayerPedId(), Vehicle.Vehicle, GetPedBoneIndex(6286), 0.0, Vehicle.Dimensions.y * -1 + 0.1 , Vehicle.Dimensions.z + 1.0, 0.0, 0.0, 180.0, 0.0, false, false, true, false, true)
+                    AttachEntityToEntity(ped, Vehicle.Vehicle, GetPedBoneIndex(6286), 0.0, Vehicle.Dimensions.y * -1 + 0.1 , Vehicle.Dimensions.z + 1.0, 0.0, 0.0, 180.0, 0.0, false, false, true, false, true)
                 else
-                    AttachEntityToEntity(PlayerPedId(), Vehicle.Vehicle, GetPedBoneIndex(6286), 0.0, Vehicle.Dimensions.y - 0.3, Vehicle.Dimensions.z  + 1.0, 0.0, 0.0, 0.0, 0.0, false, false, true, false, true)
+                    AttachEntityToEntity(ped, Vehicle.Vehicle, GetPedBoneIndex(6286), 0.0, Vehicle.Dimensions.y - 0.3, Vehicle.Dimensions.z  + 1.0, 0.0, 0.0, 0.0, 0.0, false, false, true, false, true)
                 end
 
                 RequestAnimDict('missfinale_c2ig_11')
@@ -84,11 +69,11 @@ Citizen.CreateThread(function()
                  while true do
                     Citizen.Wait(5)
                     if IsDisabledControlPressed(0, Keys["A"]) then
-                        TaskVehicleTempAction(PlayerPedId(), currentVehicle, 11, 1000)
+                        TaskVehicleTempAction(ped, currentVehicle, 11, 1000)
                     end
 
                     if IsDisabledControlPressed(0, Keys["D"]) then
-                        TaskVehicleTempAction(PlayerPedId(), currentVehicle, 10, 1000)
+                        TaskVehicleTempAction(ped, currentVehicle, 10, 1000)
                     end
 
                     if Vehicle.IsInFront then
